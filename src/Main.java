25f09744aa1ea8a2c7e9cabd778ecb99b40891c1
LFAbrahao <lfasenac@gmail.com>
2024-09-05 13:58:32 -0300

    commit exemplo saida de dados

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		double x = 10.12345;
+		System.out.printf("%.3f%n", x);
+		System.out.printf("%.4f%n", x);
 		System.out.println("Primeiro Programa do Curso");
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
 	}
 
 }
